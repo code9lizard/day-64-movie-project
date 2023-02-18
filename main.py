b698ebcb53f9c42290ff30f41241cbd9f51c5695
code9lizard <123153117+code9lizard@users.noreply.github.com>
2023-02-18 22:45:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from wtforms.validators import DataRequired
 import requests
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = 'THESECRETKEYTHATNOONEKNOWS'
 Bootstrap5(app)
 
 db = SQLAlchemy()
